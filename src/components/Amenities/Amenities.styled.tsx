3d804e22efd753c7e0a139c5ffcb93f2dd0c9202
Rodrigo Guinea <rguinea@Guineas-MacBook-Pro.local>
2019-03-30 14:21:20 -0600

    Add logo and title

--- src/components/Amenities/Amenities.styled.tsx
+++ src/components/Amenities/Amenities.styled.tsx
@@ -7,3 +7,25 @@ export const FlexContainer = styled.div`
   flex-flow: row wrap;
   align-content: center;
 `;
+
+export const Title = styled.h4`
+	font-family: SourceSansPro;
+	font-size: 27.3px;
+	font-weight: bold;
+	letter-spacing: 8.2px;
+	text-align: justify;
+	color: #da7324;
+	text-transform: uppercase;
+  text-align: center;
+  margin-bottom: 150px;
+  /* mobile phone */
+  @media all and (max-width: 768px) {
+    margin-bottom: 50px;
+	  font-size: 17.3px;
+  }
+`;
+
+export const Logo = styled.img`
+  height: 66px;
+  width: 66px;
+`;
--- src/components/Amenities/Amenities.tsx
+++ src/components/Amenities/Amenities.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 import ImageWithText from '../ImageWithText';
 
-import { FlexContainer } from './Amenities.styled';
+import { FlexContainer, Logo, Title } from './Amenities.styled';
 
 import cerradura from '../../images/cerradura.svg';
 import coche from '../../images/coche.svg';
@@ -10,6 +10,7 @@ import cocina from '../../images/cocina.svg';
 import gas from '../../images/gas.svg';
 import juegos from '../../images/juegos.svg';
 import lavadora from '../../images/lavadora.svg';
+import logo from '../../images/logo.png';
 import patio from '../../images/patio.svg';
 import toallas from '../../images/toallas.svg';
 import tv from '../../images/tv.svg';
@@ -17,6 +18,8 @@ import wifi from '../../images/wifi.svg';
 
 const Amenities: React.FC<{}> = (): JSX.Element => {
   return <>
+    <Logo src={logo}/>
+    <Title>- Servicios completos durante tu estadía -</Title>
     <FlexContainer>
       <ImageWithText imageSrc={wifi} text='Wifi en toda la estancia' />
       <ImageWithText imageSrc={tv} text='Televisión por cable' />
